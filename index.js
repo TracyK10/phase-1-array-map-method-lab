ae3f4ac9dd8e78242bf0ebbfcc71fec9abfe8ba0
Tracy Karanja <chirutracy@gmail.com>
2024-03-31 22:16:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Your job is to write the following function:
+
+// titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global tutorials variable as data.
+// NOTE: This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial. To do this, you will also need to access and modify each individual word.
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +16,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//capitalize first word in each element in the array
 const titleCased = () => {
-  return tutorials
-}
+
+    return tutorials.map(tutorial => {
+      const  words = tutorial.split(' ');
+    const capitalize = words.map(word => {return word.charAt(0).toUpperCase() + word.slice(1)})
+    return capitalize.join(' ');
+  }).join(' ')
+  
+  }
+
+
+console.log(titleCased())
